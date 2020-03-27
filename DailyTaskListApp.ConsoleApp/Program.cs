017c3d94c13d05319fccd0c841db645f39ed1a4a
Jake Gillenwater <JakeGill70@gmail.com>
2020-03-26 20:55:25 -0400

    Final - Better Console App

--- DailyTaskListApp.ConsoleApp/Program.cs
+++ DailyTaskListApp.ConsoleApp/Program.cs
@@ -19,37 +19,51 @@ namespace DailyTaskListApp.ConsoleApp
         }
 
         static void mainMenu() {
-            string[] menuChoices = new String[] { "Generate Today's Tasks List", "Exit" };
-            int myChoice = -1;
-            bool isValidNumber = false;
+            // WARNING: This code assumes that the user will usually enter correct input
+            //      on their first attempt. As such, the menuChoices dictionary is 
+            //      populated at the same time they are printed to the console.
+            //      This keeps the code performant as it only loops through the
+            //      menuAction's keys once. However, this could cause problems down the 
+            //      road if user typically enters incalid input, as the menuChoices
+            //      dictionary has to be populated menu times over.
 
-            while(true){
+            int myChoice = -1;  // User Inputted number
+            bool isValidNumber = false; // Is the user inputted number valid?
+            // Maps numbers to menu options
+            Dictionary<int, string> menuChoices = new Dictionary<int, string>();
+            // Maps menu options to actions that the program can do
+            Dictionary<string, Action> menuActions = new Dictionary<string, Action>();  
+            // Add Menu options as pairs of text to display and the action to take
+            menuActions.Add("Generate Today's Task List", new Action( () => { generateTasksList(); displayTasksList(); }));
+            menuActions.Add("Exit", new Action( () => { exit(); }));
+
+            // Loop until the user has provided valid input
+            while (!isValidNumber){
                 Console.WriteLine("MAIN MENU");
                 Console.WriteLine(DIVIDER);
-                for (int i = 0; i < menuChoices.Length; i++)
+
+                int menuChoicesIndex = 0; // Reset the indexer used to bridge menu choices to menu actions
+                foreach (string menuChoice in menuActions.Keys)
                 {
-                    Console.WriteLine((i + 1) + ". " + menuChoices[i]);
+                    menuChoicesIndex++; // Increase the index
+                    menuChoices[menuChoicesIndex] = menuChoice; // Assign that menu choice to that number
+                    Console.WriteLine(menuChoicesIndex + ". " + menuChoice); // Display the number to perform the action
                 }
 
                 Console.Write("Make a choice: ");
-                isValidNumber = int.TryParse(Console.ReadLine(), out myChoice);
-                myChoice--;
-
-                if (isValidNumber && myChoice >= 0 && myChoice < menuChoices.Length) {
-                    break;
-                }
+                // Get User input
+                isValidNumber = int.TryParse(Console.ReadLine(), out myChoice); 
+                // Validate user input
+                // isValidNumber is set to either true or false depending on if the parsing was successful.
+                // So, we want to make sure that isValidNumber is false if the user didn't even enter a number.
+                // The other validation check is to make sure that the number maps to a menu choice.
+                isValidNumber = isValidNumber && menuChoices.ContainsKey(myChoice);
             }
 
             writeBlankLine();
 
-            if (myChoice == 0)
-            {
-                generateTasksList();
-                displayTasksList();
-            }
-            else if (myChoice == 1) {
-                exit();
-            }
+            // Invoke the selected action
+            menuActions[menuChoices[myChoice]].Invoke();
             
         }
 
@@ -61,6 +75,7 @@ namespace DailyTaskListApp.ConsoleApp
 
         private static void displayTasksList()
         {
+            // Cannot display the dailyTaskList if it is not initialized yet.
             if (dailyTaskList == null) {
                 Console.WriteLine("No daily tasks list is available.");
                 return;
@@ -68,6 +83,7 @@ namespace DailyTaskListApp.ConsoleApp
 
             Console.WriteLine(dailyTaskList.Date);
             Console.WriteLine(DIVIDER);
+            // Display each task inside of dailyTaskList
             for (int i = 0; i < dailyTaskList.NumberOfTasks; i++)
             {
                 Console.WriteLine(dailyTaskList[i].StartTimeString + " " + dailyTaskList[i].Description);
@@ -77,26 +93,30 @@ namespace DailyTaskListApp.ConsoleApp
         }
 
         private static void generateTasksList() {
+            // Create a new dailyTaskList, starting today at 8:00am
             DateTime startDate = DateTime.Today;
             TimeSpan startTime = new TimeSpan(8, 0, 0);
             dailyTaskList = new DailyTaskList(startDate, startTime);
 
-            int myChoice = -1;
-            bool isValidNumber = false;
+            int numberOfSegments = -1; // The number of segments requested by the user
+            bool isValidNumber = false; // If the number of segments requested is valid
 
-            while (true)
+            while (!isValidNumber)
             {
                 Console.Write("How many half-hour segments? ");
 
-                isValidNumber = int.TryParse(Console.ReadLine(), out myChoice);
-
-                if (isValidNumber && myChoice > 0 && myChoice <= 48)
-                {
-                    break;
-                }
+                // Get user input
+                isValidNumber = int.TryParse(Console.ReadLine(), out numberOfSegments);
+                // Validate user input
+                // isValidNumber is set to either true or false depending on if the parsing was successful.
+                // So, we want to make sure that isValidNumber is false if the user didn't even enter a number.
+                // The next validation check is to ensure that the user requested at least 1 segment.
+                // The final validation check is to limit the number of possible segments to 48.
+                // It must be constrainted to 48 because that is the maximum number of 30-minute segments within a day (24hr).
+                isValidNumber = isValidNumber && numberOfSegments > 0 && numberOfSegments <= 48;
             }
 
-            dailyTaskList.Generate<TaskItem>(myChoice);
+            dailyTaskList.Generate<TaskItem>(numberOfSegments);
         }
 
         private static void writeBlankLine() {
